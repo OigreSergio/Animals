d68346ff9701615662ecfcc2195700793fb51c33
Sergio <158031668+OigreSergio@users.noreply.github.com>
2024-02-19 20:07:27 +0100

    Update Dog.java
    Adding a class Dog

--- Dog.java
+++ Dog.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Dog {
+public class Dog extends Animal{
+    public void makeNoise() {
+        System.out.println("A dog noise soud like this : \n Bauu");
+    }
 }
+
